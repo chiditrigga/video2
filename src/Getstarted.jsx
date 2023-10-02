c9dcc41369ecf510c7389351efd41f7ec6008c80
daniel <chididaniel1998@gmail.com>
2023-10-02 04:11:56 -0700

    push

--- src/Getstarted.jsx
+++ src/Getstarted.jsx
@@ -96,9 +96,9 @@ function Getstarted() {
       </Container>
      <Container fluid>
       <Row className='py-5 pt-3'>
-        <Col xs={12} style={{background:"rgba(231, 231, 237, 0.2)"}} className='text-md-center '> 
+        <Col xs={12} style={{background:"rgba(231, 231, 237, 0.2)"}} className='text-center '> 
                <h4 className='mx-auto' style={{maxWidth:"600px"}}>To ensure the availability and privacy of your video, we recommend saving it to your account.</h4>
-               <Button style={{fontWeight:"500",background:"rgba(18, 11, 72, 1)",borderColor:"rgba(18, 11, 72, 1)",fontWeight:"500"}}>Save Video</Button>
+               <Button  style={{fontWeight:"500",background:"rgba(18, 11, 72, 1)",borderColor:"rgba(18, 11, 72, 1)",fontWeight:"500"}}>Save Video</Button>
                <span><h3>Don’t have an account? <Link>Create account</Link> </h3></span>
         </Col>
       </Row>
