8170c6f2ed6d0f1f67f7d9067ffc7d2cecbc4b41
daniel <chididaniel1998@gmail.com>
2023-10-02 04:08:38 -0700

    push

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import main from "./assets/main.svg"
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Image from "react-bootstrap/Image";
 
 function Login() {
   return (
-    <div>Login</div>
+    <>
+     
+    <Container>
+<Row>
+
+    <Col xs={12}>
+     <Image fluid src={main} />
+    </Col>
+
+    
+</Row>
+
+
+
+         
+
+
+
+
+
+
+
+    </Container>
+    
+    
+    </>
   )
 }
 
